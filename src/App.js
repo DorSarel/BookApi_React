8fac01a0ac4176c54d9987a6a45a54d354190eb7
Dor Sarel <Dor.Sarel@wdc.com>
2020-01-14 22:05:14 +0200

    fill basic App content & common style properties

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default App;
+export class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <header className='header'>
+          {/* <h1 className='header__title'>Books API</h1>
+        <form className='header__form'>
+          <input className='header__search' type='search' placeholder='Enter book name' />
+        </form> */}
+        </header>
+        <main className='content'>
+          <nav className='sidebar'></nav>
+          <div className='gallery'></div>
+        </main>
+      </div>
+    );
+  }
+}
