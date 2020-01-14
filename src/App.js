b0d1e674304cd8bb7cc52c51669c7c687f7e9002
Dor Sarel <Dor.Sarel@wdc.com>
2020-01-14 22:18:16 +0200

    Wrap App content in Layout component & add basic layout style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import { Layout } from './container/Layout';
 
 export class App extends Component {
   render() {
     return (
-      <div className='container'>
+      <Layout>
         <header className='header'>
+          header
           {/* <h1 className='header__title'>Books API</h1>
         <form className='header__form'>
           <input className='header__search' type='search' placeholder='Enter book name' />
         </form> */}
         </header>
         <main className='content'>
-          <nav className='sidebar'></nav>
-          <div className='gallery'></div>
+          <nav className='sidebar'>sidebar</nav>
+          <div className='gallery'>Gallery</div>
         </main>
-      </div>
+      </Layout>
     );
   }
 }
---
+++ src/container/Layout/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.scss';
+
+export const Layout = ({ children }) => (
+  <div className='container'>{children}</div>
+);
