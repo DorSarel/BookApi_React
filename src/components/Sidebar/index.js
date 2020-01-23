6eed71bdfc7cca660e8f380063f39551fa7c821d
Dor Sarel <Dor.Sarel@wdc.com>
2020-01-23 09:35:35 +0200

    update sidebar with star-svg for favorites link

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.scss';
+import { ReactComponent as Icon } from '../../assets/img/star.svg';
 
 export const Sidebar = () => (
   <nav className='sidebar'>
     <ul className='sidebar__list'>
       <li className='sidebar__item'>
         <a href='#' className='sidebar__link'>
-          Favorites
+          <Icon />
+          <span>Favorites</span>
         </a>
       </li>
     </ul>
